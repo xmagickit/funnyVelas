7a4f32191afea5270e979375f3f2b16be1dd6901
xmagickit <silasforrest0322@gmail.com>
2025-01-08 10:49:02 -0800

    update when graduating

--- components/TokenDetail/TokenOverview.tsx
+++ components/TokenDetail/TokenOverview.tsx
@@ -6,23 +6,32 @@ import { useSocket } from "@/contexts/SocketContext";
 export default function TokenOverview({ token, vlxPrice }: { token: coinInfo, vlxPrice: number }) {
     const [progress, setProgress] = useState<number>(60);
     const { socket } = useSocket();
-    const [_token, setToken] = useState(token);
+    const [_token, setToken] = useState<coinInfo>(token);
 
     useEffect(() => {
-        const value = Math.floor((_token.price || 0) * 1_000_000_000 / (token.graduationMarketCap || 5) * 100)
+        const value = Math.floor((_token.price || 0) * 1_000_000_000 / (_token.graduationMarketCap || 5) * 100)
         setProgress(value);
     }, [_token, setProgress]);
 
     useEffect(() => {
-        const handleUpdateBondingCurve = (data: { tokenId: string, reserveOne: number, reserveTwo: number }) => {
+        const handleUpdateBondingCurve = (
+            data: {
+                tokenId: string,
+                price: number
+            }
+        ) => {
             if (_token._id === data.tokenId) {
-                const value = Math.floor((data.reserveTwo / data.reserveOne * 1_000_000_000) / (token.graduationMarketCap || 5) * 100)
+                const value = Math.floor(((data.price || 0) * 1_000_000_000) / (_token.graduationMarketCap || 5) * 100)
                 setProgress(value);
-                setToken(prevState => ({ ...prevState, reserveOne: data.reserveOne, reserveTwo: data.reserveTwo }))
+                setToken(prevState => ({ ...prevState, price: data.price }))
             }
         }
+        const handleGraduatingToDEX = (data: coinInfo) => {
+            setToken(data);
+        }
         socket?.on('update-bonding-curve', handleUpdateBondingCurve);
-
+        socket?.on('graduating-to-dex', handleGraduatingToDEX);
+        socket?.on('trading-enabled-on-uniswap', handleGraduatingToDEX);
         return () => {
             socket?.off('update-bonding-curve', handleUpdateBondingCurve);
         }
@@ -37,11 +46,11 @@ export default function TokenOverview({ token, vlxPrice }: { token: coinInfo, vl
                 </div>
                 <div className="flex flex-col gap-2">
                     {
-                        token.tradingOnUniswap ? (
-                            <p className="text-[10px] font-normal leading-normal"> uniswap pool seeded! view on uniswap <a className="text-primary" href={`https://app.uniswap.org/explore/pools/base/${token.uniswapPair}`} target="_blank">here</a></p>
-                        ) : !token.tradingPaused ? (
+                        _token.tradingOnUniswap ? (
+                            <p className="text-[10px] font-normal leading-normal"> uniswap pool seeded! view on uniswap <a className="text-primary" href={`https://app.uniswap.org/explore/pools/base/${_token.uniswapPair}`} target="_blank">here</a></p>
+                        ) : !_token.tradingPaused ? (
                             <>
-                                <p className="text-[10px] font-normal leading-normal"> When the market cap reaches ${(vlxPrice * (token.graduationMarketCap || 5)).toLocaleString()} all the liquidity from the bonding curve will be deposited into Uniswap Dex and burned. progression increases as the price goes up. </p>
+                                <p className="text-[10px] font-normal leading-normal"> When the market cap reaches ${(vlxPrice * (_token.graduationMarketCap || 5)).toLocaleString()} all the liquidity from the bonding curve will be deposited into Uniswap Dex and burned. progression increases as the price goes up. </p>
                                 {/* There are {Math.floor(1_000_000_000 - _token.reserveOne / 1_000_000)} tokens still available for sale in the bonding curve and  */}
                                 <p className="text-[10px] font-normal leading-normal">there is {(_token.reserveTwo / 1_000_000_000_000_000_000).toFixed(3)} ETH in the bonding curve. </p>
                             </>
