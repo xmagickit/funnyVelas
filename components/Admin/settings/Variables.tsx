adf5ff7422c2127af30cc1283bbe088241a24554
xmagickit <silasforrest0322@gmail.com>
2025-01-14 08:46:28 -0800

    change the text

--- components/Admin/settings/Variables.tsx
+++ components/Admin/settings/Variables.tsx
@@ -225,7 +225,7 @@ const Variables = () => {
                                         step="0.01"
                                         id="graduationMarketCap"
                                         {...register('graduationMarketCap', {
-                                            min: { value: 1.7, message: 'Graduation Market Cap can\'t be lower than 0' },
+                                            min: { value: 1.7, message: 'Graduation Market Cap can\'t be lower than 1.7 ETH' },
                                             required: 'Graduation Market Cap is required'
                                         })}
                                     />
